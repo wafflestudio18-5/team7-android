52d83cb878511839e1a763d50b6f07e17e177067
You Been Lee <yblee2001@gmail.com>
2020-12-21 19:59:26 +0900

    Timber

---
+++ app/src/main/java/com/wafflestudio/written/App.kt
@@ -0,0 +1,16 @@
+package com.wafflestudio.written
+
+import android.app.Application
+import android.os.Build
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
--- app/src/main/java/com/wafflestudio/written/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/written/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.written
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wafflestudio.written.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
