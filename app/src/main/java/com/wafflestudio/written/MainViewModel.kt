e40d157c622556eabc10eeb9f3c4c2484df0abda
veldic <rsual@snu.ac.kr>
2020-12-22 19:53:57 +0900

    Apply reviews

--- app/src/main/java/com/wafflestudio/written/MainViewModel.kt
+++ app/src/main/java/com/wafflestudio/written/MainViewModel.kt
@@ -1,10 +1,10 @@
 package com.wafflestudio.written
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
 import com.wafflestudio.written.network.TempService
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(service: TempService) {
-
-
+class MainViewModel @ViewModelInject constructor(private val service: TempService): ViewModel() {
 
 }
--- app/src/main/java/com/wafflestudio/written/di/NetworkModule.kt
+++ app/src/main/java/com/wafflestudio/written/di/NetworkModule.kt
@@ -1,19 +1,24 @@
 package com.wafflestudio.written.di
 
+import com.squareup.moshi.Moshi
 import com.wafflestudio.written.BuildConfig
 import com.wafflestudio.written.network.TempService
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 import javax.inject.Singleton
 
 @Module
+@InstallIn(ApplicationComponent::class)
 class NetworkModule {
 
     private val baseUrl = BuildConfig.WRITTEN_BASE_URL
@@ -33,10 +38,12 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi): Retrofit =
         Retrofit.Builder()
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(okHttpClient)
             .baseUrl(baseUrl)
             .build()
