4097bf9d93f031466e005a0806e2a4df2d8012ee
veldic <rsual@snu.ac.kr>
2020-12-24 18:01:10 +0900

    Fix UserServiceDto's Nullable annotations

--- app/src/main/java/com/wafflestudio/written/network/dto/user/UserSignUpResponse.kt
+++ app/src/main/java/com/wafflestudio/written/network/dto/user/UserSignUpResponse.kt
@@ -3,6 +3,6 @@ package com.wafflestudio.written.network.dto.user
 import com.wafflestudio.written.models.UserDto
 
 data class UserSignUpResponse(
-    val user: UserDto?,
-    val access_token: String?
+    val user: UserDto,
+    val access_token: String
 )
