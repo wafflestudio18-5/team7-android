8082c2a36e82162ec5f2dd7722ccf286a14aca20
veldic <rsual@snu.ac.kr>
2020-12-28 17:28:28 +0900

    Add myFragment item layout

---
+++ app/src/main/java/com/wafflestudio/written/ui/main/my/MyViewModel.kt
@@ -0,0 +1,12 @@
+package com.wafflestudio.written.ui.main.my
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.wafflestudio.written.network.service.PostingService
+import com.wafflestudio.written.network.service.UserService
+
+class MyViewModel @ViewModelInject constructor(private val userService: UserService, private val postingService: PostingService): ViewModel() {
+
+
+
+}
