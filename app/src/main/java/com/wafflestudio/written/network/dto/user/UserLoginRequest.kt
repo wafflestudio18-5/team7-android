825c51f53760e913b524b53f8e3ff5f1db301791
You Been Lee <yblee2001@gmail.com>
2021-01-07 22:02:22 +0900

    login updates

--- app/src/main/java/com/wafflestudio/written/network/dto/user/UserLoginRequest.kt
+++ app/src/main/java/com/wafflestudio/written/network/dto/user/UserLoginRequest.kt
@@ -1,6 +1,10 @@
 package com.wafflestudio.written.network.dto.user
 
+import com.squareup.moshi.Json
+
 data class UserLoginRequest(
     val facebookid: String,
     val access_token: String
+//    @field:Json(name = "access_token")
+//    val accessToken: String
 )
--- app/src/main/java/com/wafflestudio/written/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/wafflestudio/written/ui/splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.written.ui.splash
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import com.wafflestudio.written.ui.login.LoginActivity
+import com.wafflestudio.written.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,14 +13,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(LoginActivity.createIntent(this))
+//        startActivity(LoginActivity.createIntent(this))
+//        finish()
+        val nextActivity = if (PreferenceManager.getDefaultSharedPreferences(this)
+                .contains("ACCESS_TOKEN")) {
+            MainActivity.createIntent(this)
+        } else {
+            LoginActivity.createIntent(this)
+        }
+        startActivity(nextActivity)
         finish()
-//        val nextActivity = if (PreferenceManager.getDefaultSharedPreferences(this)
-//                .contains("ACCESS_TOKEN")) {
-//            MainActivity.createIntent(this)
-//        } else {
-//            LoginActivity.createIntent(this)
-//        }
     }
 
 }
