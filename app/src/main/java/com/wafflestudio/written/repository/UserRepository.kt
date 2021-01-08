b44743885fe76ea4597fd99e3b02a81f0e6cb814
veldic <rsual@snu.ac.kr>
2021-01-09 02:50:53 +0900

    Create SettingsActivity

--- app/src/main/java/com/wafflestudio/written/repository/UserRepository.kt
+++ app/src/main/java/com/wafflestudio/written/repository/UserRepository.kt
@@ -2,7 +2,10 @@ package com.wafflestudio.written.repository
 
 import com.wafflestudio.written.models.UserDto
 import com.wafflestudio.written.network.service.UserService
+import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class UserRepository (private val userService: UserService) {
 
@@ -11,5 +14,11 @@ class UserRepository (private val userService: UserService) {
     fun getUserMe(): Single<UserDto> {
         return user?.let { Single.just(it) } ?: userService.getUserMe().doOnSuccess { user = it }
     }
+
+    fun resetUser() {
+        user = null
+    }
+
+
 
 }
